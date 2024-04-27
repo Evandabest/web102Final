e2af81acaab3367cfa92dbee181528364dba4c61
Evan <evanhaque1@gmail.com>
2024-04-27 13:27:14 -0400

    fixed button

--- src/Home1.jsx
+++ src/Home1.jsx
@@ -50,7 +50,7 @@ const Home = ({supabase}) => {
         <>
         <div className="flex items-center mx-2 md:mx-12 lg:mx-40">
             <p className="flex">Filters:</p>
-            {!reversed ? ( <button className="m-auto bg-[#457EAC] rounded-md my-2 text-white p-1" onClick={reverseArray}>Oldest to newest</button>):( <button className="m-auto bg-[#457EAC] rounded-md my-2 text-white p-1" onClick={normalArray}>Newest to oldest</button>)}
+            {!reversed ? ( <button className="m-auto bg-[#457EAC] rounded-md my-2 text-white p-1" onClick={reverseArray}>Newest to oldest</button>):( <button className="m-auto bg-[#457EAC] rounded-md my-2 text-white p-1" onClick={normalArray}>Oldest to Newest</button>)}
             <button className="m-auto bg-[#457EAC] rounded-md my-2 text-white p-1" onClick={sortByLikes}>
                 Most liked
             </button>
@@ -62,13 +62,13 @@ const Home = ({supabase}) => {
                     <div className="flex flex-row bg-[#D3D0CB] rounded-md mx-2 md:mx-12 lg:mx-40" onClick={()=> navigate(`/posts/${post.id}`) } key={index}>
                         <div className="w-full" onClick={()=> navigate(`/posts/${post.id}`) }>
                         <p className="flex mx-2 font-bold">@{post.user}</p>
-                            <h1 className=" text-lg flex mx-2">{post.title}</h1>
+                            <h1 className="flex mx-2 text-lg ">{post.title}</h1>
                             {post.img ? ( 
                                 <div className="flex justify-center mx-2 my-2 md:mx-8"> {/* Added justify-center class */}
                                     <img src={post.img} className="max-w-full max-h-full" alt={post.title || 'Post Image'} /> {/* Added max-w-full and max-h-full classes */}
                                 </div>
                             ) : (
-                                <div className="text-left mx-2">
+                                <div className="mx-2 text-left">
                                     <p>{post.content}</p> 
                                 </div>
                             )}
