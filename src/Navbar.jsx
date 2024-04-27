bf800f66f9ca4b375dad37effd4608ac0d275214
Evan <evanhaque1@gmail.com>
2024-04-26 23:09:33 -0400

    added logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import './index.css'
+import logo from '/public/logo.png'
 
 
 const NavBar = () => {
@@ -25,9 +26,10 @@ const NavBar = () => {
     return (
         <>
             <div className="bg-[#457EAC] w-full flex justify-between items-center p-2">
+                <img className="w-14 md:w-20" src = {logo}/>
                 <button className="bg-[#686868] text-white p-1 rounded-lg" onClick={homeClick}>Home</button>
                 <form onSubmit={handleSubmit} className="flex-1 px-4">
-                    <input type='text' placeholder="Search by title" className="w-full rounded-lg p-1" value={search} onChange={handleChange}/>
+                    <input type='text' placeholder="Search by title" className="w-full p-1 rounded-lg" value={search} onChange={handleChange}/>
                 </form>
                 <button className="bg-[#686868] text-white p-1 rounded-lg" onClick={createClick}>Create</button>
             </div>
--- src/Results.jsx
+++ src/Results.jsx
@@ -20,12 +20,6 @@ const Results = ({supabase}) => {
         fetchPosts()
     }, [supabase, params])
 
-    const filterPosts = () => {
-        return posts.filter(post =>
-            post.title.toLowerCase().includes(params.search.toLowerCase())
-        );
-    };
-
     const addLike = async (postID, postLikes, index) => {
         var newLikes = postLikes + 1
         const {data, error} = await supabase.from("posts")
@@ -49,22 +43,49 @@ const Results = ({supabase}) => {
         setPosts(newPosts);
         setReversed(false)
     };    
+
+    const sortByLikes = () => {
+        const sortedPosts = [...posts].sort((a, b) => b.likes - a.likes);
+        setPosts(sortedPosts);
+        setReversed(true);
+    };
     
     return (
         <>
-        {!reversed ? ( <button onClick={reverseArray}>Oldest to newest</button>):( <button onClick={normalArray}>Newest to oldest</button>)}
+        <div className="flex items-center mx-2 md:mx-12 lg:mx-40">
+            <p className="flex">Filters:</p>
+            {!reversed ? ( <button className="m-auto bg-[#457EAC] rounded-md my-2 text-white p-1" onClick={reverseArray}>Oldest to newest</button>):( <button className="m-auto bg-[#457EAC] rounded-md my-2 text-white p-1" onClick={normalArray}>Newest to oldest</button>)}
+            <button className="m-auto bg-[#457EAC] rounded-md my-2 text-white p-1" onClick={sortByLikes}>
+                Most liked
+            </button>
+        </div>
             {posts.length > 0 ? (
                 posts.map((post,index) => (
-                    <div className="h-[200px]"key={index}>
-                        <div onClick={()=> navigate(`/posts/${post.id}`) }>
-                            <h1>{post.title}</h1>
-                            <p>{post.user}</p>
-                            {post.img && <img src={post.img} className="w-[400px] m-auto" alt={post.title || 'Post Image'} />}
-                            <p>{post.content}</p>
-                            <p>{post.likes}</p>
+                    <>
+                    <div className="mb-8"> 
+                    <div className="flex flex-row bg-[#D3D0CB] rounded-md mx-2 md:mx-12 lg:mx-40" onClick={()=> navigate(`/posts/${post.id}`) } key={index}>
+                        <div className="w-full" onClick={()=> navigate(`/posts/${post.id}`) }>
+                        <p className="flex mx-2 font-bold">@{post.user}</p>
+                            <h1 className="flex mx-2 text-lg ">{post.title}</h1>
+                            {post.img ? ( 
+                                <div className="flex justify-center mx-2 my-2 md:mx-8"> {/* Added justify-center class */}
+                                    <img src={post.img} className="max-w-full max-h-full" alt={post.title || 'Post Image'} /> {/* Added max-w-full and max-h-full classes */}
+                                </div>
+                            ) : (
+                                <div className="mx-2 text-left">
+                                    <p>{post.content}</p> 
+                                </div>
+                            )}
                         </div>
-                        <button onClick={() => addLike(post.id, post.likes, index)}>Like</button>
                     </div>
+                    <div className="flex flex-row bg-[#D3D0CB] rounded-md mx-2 md:mx-12 lg:mx-40" >
+                        <p className="mx-2 font-semibold">{post.likes}</p>
+                        <box-icon name='upvote'onClick={() => addLike(post.id, post.likes, index)}>Upvote</box-icon>
+                        <p className="mx-2 font-semibold">{post.commentCount}</p>
+                        <box-icon type='solid' name='comment-detail' onClick={()=> navigate(`/posts/${post.id}`) } ></box-icon>
+                    </div>
+                    </div>
+                    </>
                 ))
             ): (
                 <>
